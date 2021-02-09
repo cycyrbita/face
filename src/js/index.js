3ae3a92e839dea76952b1b69a0b84f8fdf199713
Nikita job <cycyrbita@mail.ru>
2021-02-09 18:35:53 +0300

    header и footer готовы

--- src/js/index.js
+++ src/js/index.js
@@ -1,75 +1,8 @@
 
 $(document).ready(function () {
-
-    $('#fullpage').fullpage({
-        autoScrolling: true,
-        verticalCentered: false
-	});
-
-    // AOS.init({
-    //     offset: 60
-    // });
-
-    $('.form__field').focus(function () {
-        $(this).addClass('form__field_active')
-    }).blur(function () {
-        if ($(this).val() == "") {
-            $(this).removeClass('form__field_active')
-        }
+    $('.js-nav__close').click(function() {
+        $(this).toggleClass('nav__close_active')
+        $('.js-nav__menu').toggleClass('nav__menu_active')
+        $('body, html').toggleClass('overflow')
     })
-
-    Start();
-
-    function Start() {
-        var m = 22,
-            s = 0;
-
-        if (m <= 9) {
-            m = "0" + m;
-        };
-
-        var timerId = setTimeout(function tick() {
-            if (s != 0) {
-                s = s - 1;
-
-                if (s <= 9) {
-                    s = "0" + s;
-                }
-            } else {
-                if (m != 0) {
-                    m = m - 1;
-                    s = 59;
-
-                    if (m <= 9) {
-                        m = "0" + m;
-                    }
-                } else {
-                    return
-                }
-            }
-
-            $('.form__time-count:nth-of-type(2) span:first-child').text(m);
-            $('.form__time-count:last-child span:first-child').text(s);
-            timerId = setTimeout(tick, 1000);
-        }, 1000);
-    }
-
-    $('.js-commits__box').owlCarousel({
-        loop: true,
-        autoHeight: true,
-        margin: 40,
-        responsive : {
-            320 : {
-                items: 1
-            },
-
-            768 : {
-                items: 2
-            },
-
-            1280 : {
-                items: 3
-            }
-        }
-    });
 });
